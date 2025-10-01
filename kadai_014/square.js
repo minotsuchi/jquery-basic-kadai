5274d38f8dc710737503e22ef3dfa2c386797d38
minotsuchi <m.tsuchiya.tocollo@gmail.com>
2025-10-01 12:28:25 +0900

    NEW

---
+++ kadai_014/square.js
@@ -0,0 +1,15 @@
+
+const square = (num) => {
+  return num **2;
+}
+
+
+const result = square(10);
+
+
+console.log(result);
+
+
+
+//アロー関数またはfunctionのいずれかで、引数を二乗した値を返却する関数が定義されている
+//関数の戻り値がコンソールに出力されている
